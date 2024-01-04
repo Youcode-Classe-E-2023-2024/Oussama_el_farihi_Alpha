97f43338da649c129e20e62dfa56c55803bcca2a
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-04 09:59:05 +0100

    edit db

--- _config/database.sql
+++ _config/database.sql
@@ -1,7 +1,7 @@
 -- Création de la table 'Utilisateurs'
-CREATE TABLE users (
+CREATE TABLE user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     reset_token VARCHAR(255),
