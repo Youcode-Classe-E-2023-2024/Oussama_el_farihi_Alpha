77c9c79b7902b83baaa1d2370a1b6b449099af51
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-08 09:09:46 +0100

    fix scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,18 +15,13 @@ function fetchProducts() {
   function createProductElement(product) {
     const article = document.createElement('article');
     article.className = 'flex flex-col dark:bg-gray-900';
-    
-    const img = document.createElement('img');
-    img.className = 'object-cover w-full h-52 dark:bg-gray-500';
-    img.src = 'https://source.unsplash.com/200x200/?fashion'; // Replace with your product image URL
-    img.alt = product.title; // Replace 'title' with the actual property name for image alt text
-
-    const div = document.createElement('div');
-    div.className = 'flex flex-col flex-1 p-6';
 
     const h3 = document.createElement('h3');
     h3.className = 'flex-1 py-2 text-lg font-semibold leadi';
     h3.textContent = product.title;
+    
+    const div = document.createElement('div');
+    div.className = 'flex flex-col flex-1 p-6';
 
     const userId = document.createElement('p');
   userId.textContent = `User ID: ${product.userId}`;
@@ -52,7 +47,6 @@ updateButton.onclick = function() {
     
 
     div.appendChild(h3);
-    article.appendChild(img);
     article.appendChild(div);
     article.appendChild(deleteButton);
     div.appendChild(updateButton);
